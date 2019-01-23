524b5d3b7c0446370d2254084cad4da65045864b
Ashley Colman <Ashleyc97@outlook.com>
2019-01-23 16:24:18 +0000

    Results page functionality
    The first version of the results page has been created, having a simple design with hover effects. Links currently work, the retry button redirects the user to the category selected at the first card in the set, this may need to change when the random feature is implemented. The results, image and category details are static, they need to be programmed to display the users data.

--- MathsFlipProject/results.php
+++ MathsFlipProject/results.php
@@ -22,16 +22,55 @@ session_start();
 <script src="jquery-ui-1.12.1.custom/custom-jquery-functions.js"></script> 
     
 </head>
-<body>
-    <header>
-        <div id="logo-container">
-            <img id="img-card" src="img/card.png" alt="card">
-        </div>
-        <h1 id="index-heading">MathsFlip</h1>
-        <h2 id="index-subheading">Results</h2>
-    </header>
-
     
+    <!-- Header -->
+    <header id="results-header">Results</header>
+    
+    <!-- Container of results -->
+    <div id="results-menu-container">
+        
+        <!-- Category listing -->
+    <div class="category-button category-menu-button">
+        <div class="category-icon"><img src="img/cube.png" alt="cube"></div>
+        <div class="category-name">Number</div>
+        <div class="category-percentage">15%</div>
+        </div>
+        <div class="category-scorebar"></div>
+        
+        <!-- Correct results -->
+    <div class="results-button category-menu-button" id="results-correct-button">
+        <div class="category-icon"><img src="img/tick.png" alt="tick"></div>
+        <div class="category-name">5 correct</div>
+        <div class="category-percentage">100%</div>
+        </div>
+        <div class="results-scorebar"></div>    
+        
+        <!-- Wrong results -->
+    <div class="results-button category-menu-button" id="results-wrong-button">
+        <div class="category-icon"><img src="img/cross.png" alt="cross"></div>
+        <div class="category-name">0 wrong</div>
+        <div class="category-percentage">0%</div>
+        </div>
+        <div class="results-scorebar"></div>  
+    
+    </div>
+    
+    <?php
+        // Get the category to redirect the user to the correct category of cards if hitting the retry button
+        $category = $_SESSION['category'];
+    ?>
+    
+    <!-- Buttons -->
+    <div class="results-button-container">
+        <a href="gameplay.php?category=<?php echo $category ?>&card_id=1"><button class="card-button" id="results-retry-button">Retry</button></a> <!-- Links back to the category selected with the first card in the set -->
+        <a href="categories.php"><button class="card-button" id="results-next-button">Next</button></a>
+        <a href="achievements.php"><button class="card-button" id="results-achievement-button">View Achievements</button></a>
+    </div>
+    
+    <!-- Extend page for additional scrolling at bottom of the page -->
+    <div id="results-footerspace"></div>
+<body>
+
     <footer>University Centre Weston</footer>
 </body>
 </html>
